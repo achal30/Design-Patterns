3c302d0ba3cd06f65db329f82d386b5a1c430012
Achal <apatel58@binghamton.edu>
2017-09-11 17:54:48 -0400

    comparator to sort arraylist

---
+++ myArrayList/src/myArrayList/util/MyArrayListComparator.java
@@ -0,0 +1,25 @@
+package myArrayList.util;
+
+import java.util.Comparator;
+
+public class MyArrayListComparator implements Comparator<Integer> {
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		if(o1==null){
+			return 1;
+		}
+		if(o2==null){
+			return -1;
+		}
+		if(o1 < o2){
+			return -1;
+		}
+		if(o1 > o2){
+			return 1;
+		}
+		
+		return 0;
+	}
+
+}
