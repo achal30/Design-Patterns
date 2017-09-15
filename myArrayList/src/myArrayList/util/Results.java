ba5487d0e487d34f37adb43d1bfc5a4a79443875
Achal <apatel58@binghamton.edu>
2017-09-14 20:33:48 -0400

    added new method storeNewResult() which displays the result of test cases whether they passed or failed.

--- myArrayList/src/myArrayList/util/Results.java
+++ myArrayList/src/myArrayList/util/Results.java
@@ -1,5 +1,40 @@
 package myArrayList.util;
 
-public class Results {
+import java.io.IOException;
+
+public class Results implements FileDisplayInterface, StdoutDisplayInterface {
+
+	private FileProcessor fp;
+
+	public Results(FileProcessor fpIn) {
+
+		fp = fpIn;
+
+	}
+
+	@Override
+	public void writeToFile(String s) {
+		try {
+			fp.write(s);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	@Override
+	public void writeToStdout(String s) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void storeNewResult(String testName, boolean result) throws IOException{
+		if(result){
+			fp.write("test "+testName+" passed");
+		}
+		else{
+			fp.write("test "+testName+" failed");
+		}
+	}
 
 }
