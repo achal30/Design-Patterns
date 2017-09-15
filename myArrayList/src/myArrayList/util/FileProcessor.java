a134b6411c23982b04770e464631c09b2f982b98
Achal <apatel58@binghamton.edu>
2017-09-14 22:58:05 -0400

    added javadoc style comments

--- myArrayList/src/myArrayList/util/FileProcessor.java
+++ myArrayList/src/myArrayList/util/FileProcessor.java
@@ -7,11 +7,21 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * 
+ * @author Achal
+ *
+ */
 public class FileProcessor {
-
+	 
 	private BufferedReader br;
 	private BufferedWriter bw;
 
+	/**
+	 * 
+	 * @param input
+	 * @param output
+	 */
 	public FileProcessor(String input, String output) {
 		try {
 			br = new BufferedReader(new FileReader(input));
@@ -31,16 +41,30 @@ public class FileProcessor {
 
 	}
 
+	/**
+	 * reads line from the file
+	 * @return
+	 * @throws IOException
+	 */
 	public String ReadLine() throws IOException {
 		return br.readLine();
 	}
 
+	/**
+	 * writes a line to the file
+	 * @param line
+	 * @throws IOException
+	 */
 	public void write(String line) throws IOException {
 		bw.write(line+"\n");
 		// System.out.println(line);
 	}
 
 
+	/**
+	 * closes the file
+	 * @throws IOException
+	 */
 	public void close() throws IOException {
 		br.close();
 		bw.close();
