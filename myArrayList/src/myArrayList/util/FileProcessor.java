b54c06afd564bbd359f09dc96fdcd6636785dea7
Achal <apatel58@binghamton.edu>
2017-09-14 20:20:53 -0400

    updated the file with write() method and also wrote implementation for writing to output file.

--- myArrayList/src/myArrayList/util/FileProcessor.java
+++ myArrayList/src/myArrayList/util/FileProcessor.java
@@ -1,64 +1,48 @@
 package myArrayList.util;
 
 import java.io.BufferedReader;
-//import java.io.BufferedWriter;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
-//import java.io.FileWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileProcessor {
-	
-	private FileReader fr;
+
 	private BufferedReader br;
-	//private FileWriter fw;
-	//private BufferedWriter bw;
-	
-	public FileProcessor(String input,String output){
+	private BufferedWriter bw;
+
+	public FileProcessor(String input, String output) {
 		try {
-			fr = new FileReader(input);
-			br = new BufferedReader(fr);
-			
-			//fw = new FileWriter(output);
-			//bw = new BufferedWriter(fw);
-			
+			br = new BufferedReader(new FileReader(input));
+			File f = new File(output);
+			if (f.exists()) {
+				f.delete();
+			}
+			f.createNewFile();
+
+			bw = new BufferedWriter(new FileWriter(f));
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.exit(1);
 		}
-		
-		
-	}
-	
-	public String ReadLine() throws IOException {
-		String temp=null;
-		if((temp=br.readLine())!=null)
-		return temp;
-		else{
-			temp=null;
-			return temp;
-		}
-		}
-	
-	
-	protected void finalize() throws Throwable {
-	     try {
-	         br.close();        // close open files
-	     } finally {
-	         super.finalize();
-	     }
-	 }
-		
-	//public String writetoFile(){
-	//	bw.write();
-	//	}
-		
-	/*public void close(){
-		try {
-			br.close();
-		} 
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-	}	
 
+	}
+
+	public String ReadLine() throws IOException {
+		return br.readLine();
+	}
+
+	public void write(String line) throws IOException {
+		bw.write(line+"\n");
+		// System.out.println(line);
+	}
+
+
+	public void close() throws IOException {
+		br.close();
+		bw.close();
+	}
+}
